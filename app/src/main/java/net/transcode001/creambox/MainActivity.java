1b6a6cf06239d4d825a775261c4c2580c407fbbb
transcode001 <mozcdog@gmail.com>
2017-03-12 12:02:45 +0900

    復旧完了

--- app/src/main/java/net/transcode001/creambox/MainActivity.java
+++ app/src/main/java/net/transcode001/creambox/MainActivity.java
@@ -1,13 +1,438 @@
 package net.transcode001.creambox;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ListActivity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.loopj.android.image.SmartImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import twitter4j.*;
+import twitter4j.conf.Configuration;
+
+
+public class MainActivity extends ListActivity {
+
+
+    public Twitter mTwitter;
+    private TwitterStream mTwitterStream;
+    public Configuration mConfiguration;
+    public TweetAdapter mTweetAdapter;
+    private Handler mHandler;
+    private ListView listView;
+    private Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+
+
+        if (!TwitterUtils.hasAccessToken(getApplicationContext())) {
+            Intent intent = new Intent(this, Authorization.class);
+            startActivity(intent);
+            finish();
+        } else {
+
+
+            mTweetAdapter = new TweetAdapter(getApplicationContext());
+            setListAdapter(mTweetAdapter);
+            mHandler = new Handler();
+            mConfiguration = TwitterUtils.getConfigurationInstance(this);
+            mTwitterStream = new TwitterStreamFactory(mConfiguration).getInstance();
+            mTwitter = TwitterUtils.getInstance(this);
+
+
+
+
+            //長押しの定義はListActivityではサポート外なので
+            //ツイート長押しの処理を定義
+            listView = getListView();
+
+            listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                @Override
+                public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                    LayoutInflater li = (LayoutInflater) getApplicationContext()
+                            .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                    View content = li.inflate(R.layout.popup_tweet,null);
+                    //場所をセット
+                    view.setVerticalScrollbarPosition(position);
+
+                    TextView tv = (TextView) view.findViewById(R.id.text);
+                    TextView name = (TextView) view.findViewById(R.id.name);
+
+                    builder.setView(content);
+
+                    builder.setTitle(name.getText());
+                    builder.setMessage(tv.getText());
+                    builder.create().show();
+
+                    return true;
+                }
+            });
+
+
+
+
+
+            reloadTimeLine();
+            streamTimeLine();
+
+        }
+
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_tweet:
+                Intent intent = new Intent(this, TweetActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.menu_search:
+                Intent intent_menu = new Intent(this,TweetSearch.class);
+                startActivity(intent_menu);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void reloadTimeLine() {
+        AsyncTask<Void, Void, List<twitter4j.Status>> task = new AsyncTask<Void, Void, List<twitter4j.Status>>() {
+            @Override
+            protected List<twitter4j.Status> doInBackground(Void... params) {
+                try {
+                    return mTwitter.getHomeTimeline();
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(List<twitter4j.Status> result) {
+                if (result != null) {
+                    //mTweetAdapter.clear();
+                    for (twitter4j.Status status : result) {
+                        mTweetAdapter.add(status);
+                    }
+                    //getListView().setSelection(0);
+                } else {
+                    showToast("タイムラインの取得に失敗しました");
+                }
+            }
+        };
+        task.execute();
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void streamTimeLine() {
+
+        UserStreamListener listener = new UserStreamListener() {
+            @Override
+            public void onDeletionNotice(long l, long l1) {
+
+            }
+
+            @Override
+            public void onFriendList(long[] longs) {
+
+            }
+
+            @Override
+            public void onFavorite(User user, User user1, Status status) {
+
+            }
+
+            @Override
+            public void onUnfavorite(User user, User user1, Status status) {
+
+            }
+
+            @Override
+            public void onFollow(User user, User user1) {
+
+            }
+
+            @Override
+            public void onUnfollow(User user, User user1) {
+
+            }
+
+            @Override
+            public void onDirectMessage(DirectMessage directMessage) {
+                //NotificationBuilder構築
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
+                //アイコンを設定
+                builder.setSmallIcon(R.drawable.images);
+                //テキスト設定
+                builder.setContentTitle("CreamBox");
+                builder.setContentText("You've got Direct Mail");
+                //Activityに移動(未確認)
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("hoge"));
+                PendingIntent contentIntent = PendingIntent.getActivity(getApplicationContext(),RESULT_OK,intent, PendingIntent.FLAG_ONE_SHOT);
+                builder.setContentIntent(contentIntent);
+                builder.setAutoCancel(true);
+
+                final NotificationCompat.Builder mBuilder=builder;
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
+                        manager.notify(3, mBuilder.build());
+                    }
+                });
+
+            }
+
+            @Override
+            public void onUserListMemberAddition(User user, User user1, UserList userList) {
+
+            }
+
+            @Override
+            public void onUserListMemberDeletion(User user, User user1, UserList userList) {
+
+            }
+
+            @Override
+            public void onUserListSubscription(User user, User user1, UserList userList) {
+
+            }
+
+            @Override
+            public void onUserListUnsubscription(User user, User user1, UserList userList) {
+
+            }
+
+            @Override
+            public void onUserListCreation(User user, UserList userList) {
+
+            }
+
+            @Override
+            public void onUserListUpdate(User user, UserList userList) {
+
+            }
+
+            @Override
+            public void onUserListDeletion(User user, UserList userList) {
+
+            }
+
+            @Override
+            public void onUserProfileUpdate(User user) {
+
+            }
+
+            @Override
+            public void onUserSuspension(long l) {
+
+            }
+
+            @Override
+            public void onUserDeletion(long l) {
+
+            }
+
+            @Override
+            public void onBlock(User user, User user1) {
+
+            }
+
+            @Override
+            public void onUnblock(User user, User user1) {
+
+            }
+
+            @Override
+            public void onRetweetedRetweet(User user, User user1, Status status) {
+
+            }
+
+            @Override
+            public void onFavoritedRetweet(User user, User user1, Status status) {
+
+            }
+
+            @Override
+            public void onQuotedTweet(User user, User user1, Status status) {
+
+            }
+
+            @Override
+            public void onStatus(Status status) {
+                final String tweets = "@" + status.getUser().getScreenName() + ":" + status.getText() + "\n";
+                final twitter4j.Status tweet = status;
+                System.out.println(tweets);
+
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTweetAdapter.insert(tweet, 0);
+                        //getListView().setSelection(0);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int i) {
+
+            }
+
+            @Override
+            public void onScrubGeo(long l, long l1) {
+
+            }
+
+            @Override
+            public void onStallWarning(StallWarning stallWarning) {
+
+            }
+
+            @Override
+            public void onException(Exception e) {
+
+            }
+        };
+        //インスタンスを設定
+        mTwitterStream.addListener(listener);
+        //UserStreamを開始
+        mTwitterStream.user();
+    }
+
+
+    private class TweetAdapter extends ArrayAdapter<twitter4j.Status> {
+        private LayoutInflater mInflater;
+        private URL url;
+        private InputStream mStream;
+        private Bitmap bmp;
+        private ImageView imageView;
+        private Status item;
+
+        public TweetAdapter(Context context) {
+            super(context, android.R.layout.simple_list_item_1);
+            mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.tweet_layout, null);
+            }
+
+
+            //imageView = (ImageView) convertView.findViewById(R.id.icon);
+            //Boolean getView=Boolean.FALSE;
+
+            //getUserIcon();
+
+
+            item = getItem(position);
+            TextView name = (TextView) convertView.findViewById(R.id.name);
+            name.setText(item.getUser().getName());
+            TextView screenName = (TextView) convertView.findViewById(R.id.screen_name);
+            screenName.setText("@" + item.getUser().getScreenName());
+            TextView text = (TextView) convertView.findViewById(R.id.text);
+            text.setText(item.getText());
+            SmartImageView sImageView = (SmartImageView) convertView.findViewById(R.id.icon);
+            sImageView.setImageUrl(item.getUser().getProfileImageURL());
+            TextView via=(TextView) convertView.findViewById(R.id.via);
+
+            String[] viaText = item.getSource().split("<*>",-1);
+            String[] viaTexts = viaText[1].split("<",0);
+
+            via.setText("via "+viaTexts[0]);
+
+            return convertView;
+
+
+        }
+
+        private Boolean getUserIcon() {
+            AsyncTask<Void, Void, Boolean> task = new AsyncTask<Void, Void, Boolean>() {
+                @Override
+                protected Boolean doInBackground(Void... params) {
+                    try {
+                        url = new URL(item.getUser().getProfileImageURL());
+                        mStream = url.openStream();
+                        bmp = BitmapFactory.decodeStream(mStream);
+                        //imageView.setImageBitmap(bmp);
+                        return true;
+                    } catch (MalformedURLException e) {
+                        Log.e("MalformedURLException", e.toString());
+                        return false;
+                    } catch (IOException e) {
+                        Log.e("IOException", e.toString());
+                        return false;
+                    }
+
+                }
+
+                @Override
+                protected void onPostExecute(Boolean bool) {
+                    if (bool == Boolean.TRUE) {
+                        imageView.setImageBitmap(bmp);
+                    }
+                }
+            };
+            task.execute();
+            return true;
+        }
+
+    }
+
 }
---
+++ app/src/main/java/net/transcode001/creambox/TweetSearch.java
@@ -0,0 +1,190 @@
+package net.transcode001.creambox;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.loopj.android.image.SmartImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+/**
+ * Created by kei on 17/02/06.
+ */
+
+public class TweetSearch extends Activity {
+
+    private TweetAdapter mAdapter;
+    private Twitter mTwitter;
+    private EditText et;
+    private twitter4j.Query mQuery;
+    private ListView listView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_main);
+        listView = (ListView)findViewById(R.id.listView1);
+        mAdapter = new TweetAdapter(this);
+        listView.setAdapter(mAdapter);
+        mTwitter=TwitterUtils.getInstance(this);
+        et = (EditText)findViewById(R.id.word_search);
+
+        mQuery=new twitter4j.Query();
+        findViewById(R.id.search_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!et.getText().toString().equals("")) {
+
+                    mQuery.setQuery(et.getText().toString());
+                    System.out.println("go to searchWord\n");
+                    searchWord(mQuery);
+                }
+            }
+        });
+
+    }
+
+    private void searchWord(twitter4j.Query query){
+        final twitter4j.Query mquery=query;
+        mAdapter.clear();
+        System.out.println("Before asyncTask\n");
+        final Handler mHandler = new Handler();
+        AsyncTask<Void,Void,QueryResult> task=new AsyncTask<Void, Void, QueryResult>() {
+            @Override
+            protected QueryResult doInBackground(Void... voids) {
+                try{
+                    System.out.println("hoge");
+                    final QueryResult result = mTwitter.search(mquery);
+                    System.out.println("Insert query result\n");
+                    System.out.println("Insert results\n");
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            for(twitter4j.Status status : result.getTweets()){
+                                mAdapter.add(status);
+                                System.out.println(result.getTweets().toString());
+                            }
+                        }
+                    });
+
+                }catch(TwitterException e){
+                    Log.e("Failed to search method",e.toString());
+                }
+
+                return null;
+            }
+        };
+
+        task.execute();
+
+
+
+    }
+
+
+
+
+    private class TweetAdapter extends ArrayAdapter<twitter4j.Status> {
+        private LayoutInflater mInflater;
+        private URL url;
+        private InputStream mStream;
+        private Bitmap bmp;
+        private ImageView imageView;
+        private Status item;
+        private ListView lv;
+
+
+
+        public TweetAdapter(Context context) {
+            super(context, android.R.layout.simple_list_item_1);
+            mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.tweet_layout, null);
+            }
+
+
+            //imageView = (ImageView) convertView.findViewById(R.id.icon);
+            //Boolean getView=Boolean.FALSE;
+
+            //getUserIcon();
+
+
+            item = getItem(position);
+            TextView name = (TextView) convertView.findViewById(R.id.name);
+            name.setText(item.getUser().getName());
+            TextView screenName = (TextView) convertView.findViewById(R.id.screen_name);
+            screenName.setText("@" + item.getUser().getScreenName());
+            TextView text = (TextView) convertView.findViewById(R.id.text);
+            text.setText(item.getText());
+            SmartImageView sImageView = (SmartImageView) convertView.findViewById(R.id.icon);
+            sImageView.setImageUrl(item.getUser().getProfileImageURL());
+
+            TextView via=(TextView) convertView.findViewById(R.id.via);
+
+            String[] viaText = item.getSource().split("<*>",-1);
+            String[] viaTexts = viaText[1].split("<",0);
+
+            via.setText("via "+viaTexts[0]);
+            return convertView;
+
+
+        }
+
+        /*private Boolean getUserIcon() {
+            AsyncTask<Void, Void, Boolean> task = new AsyncTask<Void, Void, Boolean>() {
+                @Override
+                protected Boolean doInBackground(Void... params) {
+                    try {
+                        url = new URL(item.getUser().getProfileImageURL());
+                        mStream = url.openStream();
+                        bmp = BitmapFactory.decodeStream(mStream);
+                        //imageView.setImageBitmap(bmp);
+                        return true;
+                    } catch (MalformedURLException e) {
+                        Log.e("MalformedURLException", e.toString());
+                        return false;
+                    } catch (IOException e) {
+                        Log.e("IOException", e.toString());
+                        return false;
+                    }
+                }
+                @Override
+                protected void onPostExecute(Boolean bool) {
+                    if (bool == Boolean.TRUE) {
+                        imageView.setImageBitmap(bmp);
+                    }
+                }
+            };
+            task.execute();
+            return true;
+        }*/
+
+    }
+}
