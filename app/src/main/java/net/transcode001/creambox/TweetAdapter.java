f4bf40e72cfd05aa0e51765ad1c36b3c8150d1a4
transcode001 <mozcdog@gmail.com>
2017-10-04 23:44:26 +0900

    add:画像表示のレイアウトを定義

--- app/src/main/java/net/transcode001/creambox/TweetAdapter.java
+++ app/src/main/java/net/transcode001/creambox/TweetAdapter.java
@@ -2,10 +2,16 @@ package net.transcode001.creambox;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.renderscript.ScriptGroup;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,6 +19,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -39,7 +46,7 @@ public class TweetAdapter extends ArrayAdapter<twitter4j.Status>{
         private ListView lv;
         private RecyclerView.ViewHolder holder;
         private ContextUtils contUtils;
-
+        private LinearLayout linearLayout;
 
 
         public TweetAdapter(Context context) {
@@ -54,11 +61,10 @@ public class TweetAdapter extends ArrayAdapter<twitter4j.Status>{
                 convertView = mInflater.inflate(R.layout.tweet_layout, null);
             }
 
+            /*以前保持した画像があれば削除*/
+            linearLayout = (LinearLayout)convertView.findViewById(R.id.media);
+            if(linearLayout.getChildCount()>0) linearLayout.removeAllViews();
 
-            //imageView = (ImageView) convertView.findViewById(R.id.icon);
-            //Boolean getView=Boolean.FALSE;
-
-            //getUserIcon();
             Status item;
             TextView text = (TextView) convertView.findViewById(R.id.text);
             if(getItem(position).isRetweet()){
@@ -67,32 +73,74 @@ public class TweetAdapter extends ArrayAdapter<twitter4j.Status>{
             }else{
                 item = getItem(position);
                 text.setTextColor(Color.BLACK);
-                //getUserIcon(getItem(position));
             }
 
-
-
-
+            /*ID表示*/
             TextView name = (TextView) convertView.findViewById(R.id.name);
             name.setText(item.getUser().getName());
             TextView screenName = (TextView) convertView.findViewById(R.id.screen_name);
             screenName.setText("@" + item.getUser().getScreenName());
             text.setText(item.getText());
 
+            /*アイコン表示*/
             Bitmap bmps = IconCacheUtils.getIcon((getItem(position).isRetweet()) ?
                     getItem(position).getRetweetedStatus().getUser().getScreenName():getItem(position).getUser().getScreenName());
             if(bmps==null) getUserIcon(item,convertView,item.getUser().getScreenName());
             else ((ImageView)convertView.findViewById(R.id.icon)).setImageBitmap(bmps);
 
-            TextView via=(TextView) convertView.findViewById(R.id.via);
+            /*Media取得*/
+            MediaEntity[] mediaEntity = item.getExtendedMediaEntities();
+            if(mediaEntity.length>0) {
+                for (MediaEntity media : mediaEntity) {
+                    ImageView mediaView = new ImageView(getContext());
+                    System.out.println("resource:"+media.getMediaURL());
+                    /*Bitmap d = getImage(media.getMediaURL());
+                    mediaView.setImageBitmap(d);
+                    */
+                    Uri uri = Uri.parse(media.getMediaURL());
+                    mediaView.setImageURI(uri);
+                    LinearLayout.LayoutParams params =
+                            new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    params.setMarginEnd(15);
+                    mediaView.setLayoutParams(params);
+                    linearLayout.addView(mediaView,params);
+                }
+            }
 
+            /*via表示*/
+            TextView via=(TextView) convertView.findViewById(R.id.via);
             String[] viaText = item.getSource().split("<*>",-1);
             String[] viaTexts = viaText[1].split("<",0);
-
             via.setText("via "+viaTexts[0]);
+
             return convertView;
 
+
         }
+
+        private Bitmap getImage(String imageURL){
+            Drawable d;
+            InputStream is;
+            Bitmap b;
+            try{
+                URL url = new URL(imageURL);
+                Object o =url.getContent();
+                //is =(InputStream) url.getContent();
+                //d = Drawable.createFromStream(is,"");
+                b = BitmapFactory.decodeResource((Resources)o,0);
+                //is.close();
+            }catch(MalformedURLException me){
+                System.out.println(me.toString());
+                b=null;
+            }catch(IOException ioe){
+                System.out.println(ioe.toString());
+                b=null;
+            }
+            return b;
+
+        }
+
+
 
         private Boolean getUserIcon(twitter4j.Status status, View view,String userScreenName) {
             final twitter4j.Status userStatus = status;
