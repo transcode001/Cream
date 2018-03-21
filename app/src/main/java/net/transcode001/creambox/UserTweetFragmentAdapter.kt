f260a0dfb8c14e2bb02d2fa97f4ea4a792afdd38
transcode001 <mozcdog@gmail.com>
2018-03-22 02:41:53 +0900

    getItem内のelse-ifをwhenに記法を変更

--- app/src/main/java/net/transcode001/creambox/UserTweetFragmentAdapter.kt
+++ app/src/main/java/net/transcode001/creambox/UserTweetFragmentAdapter.kt
@@ -7,20 +7,25 @@ import android.support.v4.app.FragmentPagerAdapter
 
 
 open class UserTweetFragmentAdapter(fm:FragmentManager,id:Long): FragmentPagerAdapter(fm){
-    val PAGE_NUM =2
+    private val PAGE_NUM =2
     val id:Long
     init{
         this.id = id
     }
 
     override fun getItem(position: Int): Fragment {
-        if(position==1){
-            val args = Bundle()
-            val utf = UserTweetFragment()
-            args.putLong("userid",id)
-            utf.arguments = args
-            return utf
-        }else return TFragment()
+        val fragment:Fragment
+        when(position){
+            0 ->{
+                val args =Bundle()
+                val utf = UserTweetFragment()
+                args.putLong("userid",id)
+                utf.arguments = args
+                fragment = utf
+            }
+            else -> fragment = TFragment()
+        }
+        return fragment
     }
 
     override fun getCount(): Int {
